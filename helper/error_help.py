fixed bugs, tested pca reconstruction method (works better the @), fixed rmse calculation to get the accuracy in the reconstruction

helper/error_help.py
@@ -68,14 +68,21 @@ def rmse(X, X_estimated):
         rmse_value: RMSE value
     """
 
-    N = X.shape[0]                                          # Number of features
-    frob_err, frob_percent = frobenius_error(X, X_estimated)     # Frobenius error and percentage
-    
-    rmse_value = np.sqrt(frob_err/N)  # RMSE calculation
+    X = np.array(X)
+    X_estimated = np.array(X_estimated)
 
-    return rmse_value
+    if X.shape != X_estimated.shape:
+        raise ValueError("Input arrays must have the same shape.")
+    
+    mse = np.mean((X-X_estimated)**2)
+
+    rmse = np.sqrt(mse)
+
+    return 1 - rmse
 
 
 
 #--------------------------------------------------------------------------------------------
+
+
 

helper/pca_help.py
@@ -19,22 +19,33 @@ def apply_pca(emg_data, n_components, scale_U=False, random_state=None, svd_solv
         explained_variance: Variance explained by each component
     """
 
+    #preprocessing data for a clear reconstruction (centering and normalization)
+    X_centered = emg_data - np.mean(emg_data, axis=0)
+    X_normalized = (emg_data - np.mean(emg_data, axis=0)) / np.std(emg_data, axis=0)
+
+    #model pca
     pca = PCA(n_components=n_components, svd_solver=svd_solver, random_state=random_state)
+    
+    #extracting matrices
     U = pca.fit_transform(emg_data)             # Synergies over time
     S_m = pca.components_                       # Muscle patterns (Muscular synergy matrix)
-
     mean = pca.mean_
-
-    if scale_U:
-        # Scale scores by explained variance (makes them more comparable)
-        U = U * np.sqrt(pca.explained_variance_ratio_)
     
+    # Scale scores by explained variance (makes them more comparable)
+    if scale_U:
+        U = U * np.sqrt(pca.explained_variance_ratio_)
     # Transpose to keep same structure as NMF function
     if S_m.shape[0] != n_components:
         S_m = S_m.T     # Ensure S_m has shape (n_synergies, n_muscles)
     
+    #reconstruction based on the inverse transform
+    X_transformed = pca.fit_transform(X_centered) # Neural matrix (synergies over time) adjusted for centering wrt original data and enforce positive values
+    X_reconstructed = pca.inverse_transform(X_transformed) + np.mean(emg_data, axis=0) # the mean is added to enforce values of synergies and reconstruction being non negative as the original data
 
-    return S_m, U, mean
+    """mse = np.mean((emg_data - X_reconstructed) ** 2)
+    print(f"Reconstruction MSE: {mse}")"""
+
+    return S_m, U, mean, X_reconstructed
 
 
 